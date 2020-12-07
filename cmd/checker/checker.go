9f1209a9c210b3a727eac306d494aff1680ab807
Gian Carlo Pace <giancarlo.pace@gmail.com>
2020-12-07 18:43:27 +0100

    adding an external package

--- cmd/checker/checker.go
+++ cmd/checker/checker.go
@@ -1,7 +1,9 @@
 package main;
 
-import  "fmt";
+import "fmt";
+import "file";
 
 func main() {
 	fmt.Println("Test");
+	file.LoadQuiz();
 }
---
+++ src/file/load.go
@@ -0,0 +1,7 @@
+package file;
+
+import "fmt";
+
+func LoadQuiz() {
+	fmt.Printf("loading file");
+}
